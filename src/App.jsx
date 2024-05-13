07e06d22115f4ebd0b8408c9eb359ebcaec2c743
Christian Kuechler <ckuechler@gmail.com>
2024-05-13 11:45:41 +0200

    Display albums in responsive grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Albums from "./assets/pages/Albums";
 
 export default function App() {
   return (
-    <div className="container mx-auto px-100">
+    <div className="container mx-auto">
       <h1 className="text-3xl font-bold underline">RecordLoverz</h1>
       <Albums />
     </div>
