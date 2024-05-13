e6f2e5ffa067647a0c0c995720fb8249a00d220b
Christian Kuechler <ckuechler@gmail.com>
2024-05-13 15:13:17 +0200

    Searchbar demo

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,31 +1,31 @@
 function Navbar() {
-    return(
-        <nav
-      class="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-slate-600 border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
-      <div class="flex items-center justify-between text-blue-gray-900">
-        <a href="#"
-          class="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
-          <h1>Record Loverz</h1>
-        </a>
-        <div class="flex items-center gap-4">
-          <div class="hidden mr-4 lg:block">
-            <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-              <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                <a href="#" class="flex items-center">
-                  Add Album
-                </a>
-              </li>
-              <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                <a href="#" class="flex items-center">
-                  Home
-                </a>
-              </li>
-            </ul>
-          </div>
-
+  return(
+      <nav
+    className="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-slate-600 border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
+    <div className="flex items-center justify-between text-blue-gray-900">
+      <a href="#"
+        className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
+        <h1>Record Loverz</h1>
+      </a>
+      <div className="flex items-center gap-4">
+        <div className="hidden mr-4 lg:block">
+          <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+              <a href="#" className="flex items-center">
+                Add Album
+              </a>
+            </li>
+            <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+              <a href="#" className="flex items-center">
+                About
+              </a>
+            </li>
+          </ul>
         </div>
+
       </div>
-    </nav>
-    );
+    </div>
+  </nav>
+  );
 }
 export default Navbar;
--- src/assets/pages/Albums.jsx
+++ src/assets/pages/Albums.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { API_URL } from "../constants";
+import Search from "../components/Search";
 
 function Albums() {
   const [albums, setAlbums] = useState(null);
@@ -14,19 +15,40 @@ function Albums() {
       .catch((e) => console.log(e));
   }, []);
 
+  function searchAlbumOrArtist(query) {
+    console.log(query);
+    const needle = query.toLowerCase();
+    const searchResult = albums.filter((album) => {
+      const artistName = album.artist.toLowerCase();
+      const albumName = album.artist.toLowerCase();
+      if (artistName.indexOf(needle) !== -1 || albumName.indexOf(needle) !== -1) {
+        return true;
+      }
+    });
+    setAlbums(searchResult);
+    console.log(searchResult);
+  }
+
   return (
-    <section className="grid grid-cols-3 gap-10">
-      {albums &&
-        albums.map((album, index) => {
-          return (
-            <div className="p-4 rounded-lg shadow-lg bg-indigo-500" key={index}>
-              <img src={album.image_url} alt={album.title} />
-              {album.artist} <br />
-              {album.title}
-            </div>
-          );
-        })}
-    </section>
+    <>
+      <Search searchAlbumOrArtist={searchAlbumOrArtist} />
+      
+      <section className="grid grid-cols-3 gap-10">
+        {albums &&
+          albums.map((album, index) => {
+            return (
+              <div
+                className="p-4 rounded-lg shadow-lg bg-slate-400 drop-shadow-xl"
+                key={index}
+              >
+                <img src={album.image_url} alt={album.title} />
+                {album.artist} <br />
+                {album.title}
+              </div>
+            );
+          })}
+      </section>
+    </>
   );
 }
 
