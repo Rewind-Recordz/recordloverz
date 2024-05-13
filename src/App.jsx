7a99bb86d604ae5172d524448a43b33b733b3a03
Christian Kuechler <ckuechler@gmail.com>
2024-05-13 10:51:37 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <div className="bg-cyan-300">hello</div>
+    </>
+    
+  )
+}
---
+++ src/assets/constants.js
@@ -0,0 +1,2 @@
+const API_URL = "https://musicloverz-backend.adaptable.app/albums";
+export {API_URL};
