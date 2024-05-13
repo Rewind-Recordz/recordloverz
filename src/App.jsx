1c9ad932f626dcb865d04fa969f9639fbe330935
Christian Kuechler <ckuechler@gmail.com>
2024-05-13 12:10:03 +0200

    Added components folder. Added sticky navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Navbar from "./assets/components/Navbar";
 import Albums from "./assets/pages/Albums";
 
 export default function App() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline">RecordLoverz</h1>
-      <Albums />
-    </div>
+    <>
+      <Navbar />
+      {/* responsive container */}
+      <div className="container mx-auto">
+        <Albums />
+      </div>
+    </>
   );
 }
