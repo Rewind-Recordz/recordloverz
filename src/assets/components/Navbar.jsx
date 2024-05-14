56b0810a139691cfd3b405640181446d027b5e81
Christian Kuechler <ckuechler@gmail.com>
2024-05-14 18:49:55 +0200

    Login Form and logic

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,37 +1,41 @@
 import { Link } from "react-router-dom";
+import { AuthContext } from "../components/AuthContext";
+import { useContext } from "react";
 
 function Navbar() {
-  return(
-      <nav
-    className="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-slate-600 border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
-    <div className="flex items-center justify-between text-blue-gray-900">
-      <Link to="/" className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
-        <h1>Record Loverz</h1>
-      </Link>
-      <div className="flex items-center gap-4">
-        <div className="hidden mr-4 lg:block">
-          <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-          <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-              <Link to="/albums" className="flex items-center">
-                Albums
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+  return (
+    <nav className="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-slate-600 border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <Link
+          to="/"
+          className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased"
+        >
+          <h1>Record Loverz</h1>
+        </Link>
+        <div className="flex items-center gap-4">
+          <div className="hidden mr-4 lg:block">
+            <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                <Link to="/albums" className="flex items-center">
+                  Albums
                 </Link>
-            </li>
-            <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-              <Link to="/login" className="flex items-center">
-                Login
+              </li>
+              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                <Link to="/login" className="flex items-center">
+                  {isLoggedIn ? "Logout" : "Login"}
                 </Link>
-            </li>
-            <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-              <a href="#" className="flex items-center">
-                About
-              </a>
-            </li>
-          </ul>
+              </li>
+              <li className="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                <a href="#" className="flex items-center">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
 export default Navbar;
--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -1,22 +1,76 @@
-
-import { useContext } from 'react';
-import { AuthContext } from '../components/AuthContext';
-import { API_URL } from "../constants";
+import { useContext, useState } from "react";
+import { AuthContext } from "../components/AuthContext";
+import { ADMIN_LOGIN, ADMIN_PASSWORD, API_URL } from "../constants";
 
 function Login() {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  function handleLogin(login) {
-    // Perform login logic
-    setIsLoggedIn(login);
-  };
+  function handleLogin() {
+    if (username === ADMIN_LOGIN && password === ADMIN_PASSWORD) {
+      setIsLoggedIn(true);
+    }
+  }
+
+  function logOut() {
+    setIsLoggedIn(false);
+  }
 
   return (
     <>
-      <h1>Login Page</h1>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => handleLogin(true)}>Login</button>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => handleLogin(false)}>Logout</button>
-      <div>{ isLoggedIn ? "Logged in" : "Logged out"}</div>
+      <div className="flex justify-center mt-10">
+        <div className="w-full max-w-xs">
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                name="username"
+                type="text"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              {!isLoggedIn && (
+                <button
+                  onClick={handleLogin}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                >
+                  Login
+                </button>
+              )}
+
+              {isLoggedIn && (
+                <button
+                  className="h-10 px-5 m-2 text-red-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800"
+                  onClick={() => logOut()}
+                >
+                  Logout
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+      <br />
+      <br />
     </>
   );
 }
