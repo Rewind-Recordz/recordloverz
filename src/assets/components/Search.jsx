d7faaaff51f800e6db2d0e7145e0bbe6f615ee93
Sergio Diaz <sadiazcaballero@gmail.com>
2024-05-16 13:57:38 +0200

    Changed disco to pop

--- src/assets/components/Search.jsx
+++ src/assets/components/Search.jsx
@@ -76,7 +76,7 @@ function Search(props) {
           }}
           className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-700 dark:border-gray-700"
         >
-          Disco
+          Pop
         </button>
         <button
           onClick={() => {
